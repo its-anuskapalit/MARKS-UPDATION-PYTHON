ccf00cdd9c064fd8c8c20e5ebf7ac752f0b01c1a
Anuska Palit <113778308+its-anuskapalit@users.noreply.github.com>
2022-12-01 19:27:56 +0530

    Update MARKS UPDATION.py

--- MARKS UPDATION.py
+++ MARKS UPDATION.py
@@ -1,16 +1,14 @@
 def nameRank(names, marks, updates, n):
-x = [[0 for j in range(3)] for i in range(n)]
-for i in range(n):
-x[i][0] = names[i]
-x[i][1] = marks[i] + updates[i]
-x[i][2] = i + 1
-
-highest = x[0]
-for j in range(1, n):
-if (x[j][1] >= highest[1]):
-highest = x[j]
-print("Name: ", highest[0], ", Jump: ",
-abs(highest[2] - 1), sep="")
+    x = [[0 for j in range(3)] for i in range(n)]
+    for i in range(n):
+        x[i][0] = names[i]
+        x[i][1] = marks[i] + updates[i]
+        x[i][2] = i + 1
+        highest = x[0]
+    for j in range(1, n):
+        if (x[j][1] >= highest[1]):
+            highest = x[j]
+        print("Name: ", highest[0], ", Jump: ",abs(highest[2] - 1), sep="")
 
 
 names = ["Sneha", "Raj", "Riya"]
